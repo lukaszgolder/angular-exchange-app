730f4115d3b35bb3ea6375493f094eb483745db3
Łukasz Golder <lukasz@golderit.pl>
2016-05-19 19:58:08 +0200

    init

---
+++ app/app.routes.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app')
+    .config(routes);
+
+  routes.$inject = ['$routeProvider'];
+
+  /* @ngInject */
+  function routes($routeProvider) {
+    
+  }
+
+})();
